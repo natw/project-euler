60934392c527489367021802f08ebfd828b27a3a
Nat Williams <nwilliams@leapfrogonline.com>
2015-04-23 11:31:25 -0500

    prob 21 down to under a second

--- 21.scala
+++ 21.scala
@@ -9,23 +9,10 @@ def divisors(i: Int): Seq[Int] = {
 
 def d(n: Int): Int = { divisors(n).sum }
 
-def areAmicable(i: Int, j: Int): Boolean = { d(i) == j && d(j) == i }
-
 def amicableNumbers(limit: Int): Seq[Int] = {
-  def _am(start: Int) = {
-    (start+1 to limit).filter(areAmicable(_, start)).flatMap(List(start, _))
-  }
-  return (1 to limit).flatMap(_am _)
+  (1 to limit).map { i: Int => List(i, d(i)) }
+              .filter { case List(i:Int, j:Int) => i < j && d(j) == i }
+              .flatten
 }
 
-println(divisors(220))
-println(d(220))
-println(divisors(284))
-println(d(284))
-println(areAmicable(220, 284))
-
-println(amicableNumbers(500))
-
-var amNums: Seq[Int] = amicableNumbers(10000)
-println(amNums.sum)
-
+println(amicableNumbers(10000).sum)
