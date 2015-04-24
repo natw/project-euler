3d4bcfd3822455e2f15f3c82c24816dda8fa7f2f
Nat Williams <nwilliams@leapfrogonline.com>
2015-04-24 09:29:37 -0500

    quicker, but wrong

--- 23.scala
+++ 23.scala
@@ -6,39 +6,23 @@ def divisors(i: Int): Seq[Int] = {
   (1 to sqrt(i).toInt).filter(i % _ == 0).flatMap{ d: Int =>  List(d, i/d) }.filter(_ < i)
 }
 
+def isAbundant(i: Int): Boolean = {
+  divisors(i).sum > i
+}
+
 def abundantNumbers(min: Int, max: Int): Seq[Int] = {
-  (min to max).filter{ i: Int => divisors(i).sum > i }
+  (min to max).filter(isAbundant(_))
 }
 
 def isSumOfAbundantNums(i: Int): Boolean = {
-  var aNums = abundantNumbers(1, i)
-  var pairs = aNums.combinations(2) ++ aNums.zip(aNums).map{ z: (Int, Int) => Vector(z._1, z._2) }
-  pairs.foreach { pair => 
-    if(pair.sum == i) { return true }
+  (1 to i).foreach { n =>
+    if(isAbundant(n) && isAbundant(i-n)) { return true }
   }
   return false
 }
 
-def anySum(i: Int, aNums: Seq[Int]): Boolean = {
-  aNums.foreach { a1 =>
-    aNums.foreach { a2 =>
-      if(a1 + a2 == i) { return true }
-    }
-  }
-  false
+def nonAbundantSums(limit: Int): Seq[Int] = {
+  (0 to limit).filterNot(isSumOfAbundantNums).toList
 }
 
-def noAbundantSum(max: Int): Seq[Int] = {
-  var noSum: List[Int] = List()
-  var aNums = abundantNumbers(1, max)
-  var pairs = aNums.combinations(2).toList ++ aNums.map{ n: Int => List(n, n) }
-  println(pairs)
-  var sums = pairs.map(_.sum).distinct
-  println(s"sums: $sums")
-  var nums = (1 to max).toList
-  println(s"nums: $nums")
-  nums.diff(sums)
-}
-
-println(noAbundantSum(25))
-println(noAbundantSum(28123))
+println(nonAbundantSums(28123).sum)
