533756e1f2ac7128d86f92086799fb9996a1cc8e
Nat Williams <nwilliams@leapfrogonline.com>
2015-04-24 09:53:55 -0500

    copied from the forum. should try to make it my own, but I'm bored

--- 23.scala
+++ 23.scala
@@ -3,8 +3,13 @@ import scala.Iterator
 
 
 def divisors(i: Int): Seq[Int] = {
-  (1 to sqrt(i).toInt).filter(i % _ == 0).flatMap{ d: Int =>  List(d, i/d) }.filter(_ < i)
+  (1 to sqrt(i).toInt).filter(i % _ == 0).flatMap{ d: Int =>  List(d, i/d) }.filter(_ < i).distinct
 }
+
+def divisors2(x : Int): List[Int] =
+  1 :: (for(i <- 2 to Math.sqrt(x).toInt if x % i == 0) yield {
+    List(i,(x/i).toInt)
+  }).toList.flatten.distinct
 
 def isAbundant(i: Int): Boolean = {
   divisors(i).sum > i
@@ -14,15 +19,19 @@ def abundantNumbers(min: Int, max: Int): Seq[Int] = {
   (min to max).filter(isAbundant(_))
 }
 
-def isSumOfAbundantNums(i: Int): Boolean = {
-  (1 to i).foreach { n =>
-    if(isAbundant(n) && isAbundant(i-n)) { return true }
+def sumOfNonAbundantSums(limit: Int): Int = {
+  var aNums = abundantNumbers(1, limit)
+  var nums = Array.range(1, limit+1)
+  for(
+    i <- aNums;
+    j <- aNums.dropWhile(_ != i ) if i + j <= limit
+  ) {
+    nums(i+j-1) = 0
   }
-  return false
+  nums.sum
 }
 
-def nonAbundantSums(limit: Int): Seq[Int] = {
-  (0 to limit).filterNot(isSumOfAbundantNums).toList
-}
 
-println(nonAbundantSums(28123).sum)
+println(sumOfNonAbundantSums(28123))
+
+
